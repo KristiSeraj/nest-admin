429fac9628cbb46f18f4fd9b3326e4f1679bf811
KristiSeraj <serajkristi@gmail.com>
2024-02-27 23:10:39 +0100

    has permission created and a lot of stuff

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PermissionService } from './permission.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('permissions')
 export class PermissionController {
   constructor(private permissionService: PermissionService) {}
+
   @Get()
   async all() {
     return this.permissionService.all();
