680ae8ac41ae1f698af854abc9d094f85d5bb3db
KristiSeraj <serajkristi@gmail.com>
2024-02-27 23:09:55 +0100

    has permission wrapper added to role conteollers and also role exported

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -8,17 +8,20 @@ import {
   Post,
 } from '@nestjs/common';
 import { RoleService } from './role.service';
+import { HasPermission } from 'src/permission/has-permission.decorator';
 
 @Controller('roles')
 export class RoleController {
   constructor(private roleService: RoleService) {}
 
   @Get()
+  @HasPermission('roles')
   async all() {
     return this.roleService.all();
   }
 
   @Post()
+  @HasPermission('roles')
   async create(@Body('name') name: string, @Body('permissions') ids: number[]) {
     return this.roleService.create({
       name,
@@ -27,11 +30,13 @@ export class RoleController {
   }
 
   @Get(':id')
+  @HasPermission('roles')
   async get(@Param('id') id: number) {
     return this.roleService.findOne({ id });
   }
 
   @Put(':id')
+  @HasPermission('roles')
   async update(
     @Param('id') id: number,
     @Body('name') name: string,
@@ -48,6 +53,7 @@ export class RoleController {
   }
 
   @Delete(':id')
+  @HasPermission('roles')
   async delete(@Param('id') id: number) {
     return this.roleService.delete(id);
   }
