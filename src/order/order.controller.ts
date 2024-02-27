233dde8bfc973a9929f86b82d3ccc8173a4b564e
KristiSeraj <serajkristi@gmail.com>
2024-02-27 23:10:18 +0100

    product and order with has permission wrapper

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -14,6 +14,7 @@ import { Response } from 'express';
 import { Parser } from 'json2csv';
 import { Order } from './order.entity';
 import { OrderItem } from './order-item.entity';
+import { HasPermission } from 'src/permission/has-permission.decorator';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
@@ -22,11 +23,13 @@ export class OrderController {
   constructor(private orderService: OrderService) {}
 
   @Get('orders')
+  @HasPermission('orders')
   async all(@Query('page') page = 1) {
     return this.orderService.paginate(page, ['order_items']);
   }
 
   @Post('export')
+  @HasPermission('orders')
   async export(@Res() res: Response) {
     const parser = new Parser({
       fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'],
@@ -65,6 +68,7 @@ export class OrderController {
   }
 
   @Get('chart')
+  @HasPermission('orders')
   async chart() {
     return this.orderService.chart();
   }
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -13,6 +13,7 @@ import { ProductService } from './product.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { ProductCreateDto } from './models/product-create.dto';
 import { ProductUpdateDto } from './models/product-update.dto';
+import { HasPermission } from 'src/permission/has-permission.decorator';
 
 @UseGuards(AuthGuard)
 @Controller('products')
@@ -20,21 +21,25 @@ export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Get()
+  @HasPermission('products')
   async all(@Query('page') page = 1) {
     return this.productService.paginate(page);
   }
 
   @Post()
+  @HasPermission('products')
   async create(@Body() body: ProductCreateDto) {
     return this.productService.create(body);
   }
 
   @Get(':id')
+  @HasPermission('products')
   async get(@Param('id') id: number) {
     return this.productService.findOne({ id });
   }
 
   @Put(':id')
+  @HasPermission('products')
   async update(@Param('id') id: number, @Body() body: ProductUpdateDto) {
     await this.productService.update(id, body);
 
@@ -42,6 +47,7 @@ export class ProductController {
   }
 
   @Delete(':id')
+  @HasPermission('products')
   async delete(@Param('id') id: number) {
     return this.productService.delete(id);
   }
